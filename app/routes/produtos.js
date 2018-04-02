358265e2c0368efd8d8150c12954abda1c31bc29
magno carvalho <magno.carvalho@ykt.com.br>
2018-04-02 17:56:07 -0300

    aula 5

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -13,18 +13,37 @@ module.exports = function(app) {
 
     app.get('/produtos', listaProdutos);
 
+    app.get('/produtos/json',function(req,res){
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        produtosDAO.lista(function(erros,resultados){
+            res.json(resultados);
+        });
+        connection.end();
+    });
+
     app.get('/produtos/form', function(req, res) {
-        res.render('produtos/form');
+        res.render('produtos/form', {errosValidacao:{}, produto:{}});
     });
 
     app.post('/produtos', function(req, res) {
         var produto = req.body;
+        console.log(produto);
 
         var connection = app.infra.connectionFactory();
-        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        var produtosDao = new app.infra.ProdutosDAO(connection);
 
-        produtosDAO.salva(produto, function(err, results) {
-            res.redirect('/produtos');
+        req.assert('titulo', 'Titulo deve ser preenchido').notEmpty();
+        req.assert('preco','Formato inválido').isFloat();
+
+        var errors = req.validationErrors();
+        if(errors){
+            res.render('produtos/form', {errosValidacao : errors, produto:produto});
+            return;
+        }
+
+        produtosDao.salva(produto,function(erros,resultado){
+                res.redirect("/produtos");
         });
 
         connection.end();
---
+++ cadastra.js
@@ -0,0 +1,28 @@
+var http = require('http');
+
+var configuracoes = {
+    hostname: 'localhost',
+    port: 3000,
+    method: 'post',
+    path: '/produtos',
+    headers: {
+        'Accept': 'application/json',
+        'Content-type':'application/json'
+    
+    }
+};
+
+var cliente = http.request(configuracoes, function(res){
+    console.log(res.statusCode);
+    res.on('data', function(body){
+        console.log('Corpo:' +body);
+    });
+});
+
+var produto = {
+    titulo : 'mais sobre node',
+    descricao: 'node, javascript e um pouco sobre http',
+    preco: '100'
+}
+
+cliente.end(JSON.stringify(produto));
